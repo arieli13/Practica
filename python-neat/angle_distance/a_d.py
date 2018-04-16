697a162d1b1028e3f4c6abcb22034f34ed9a8f75
arieli13 <arieli13.10@gmail.com>
2018-04-16 18:48:29 +0200

    Mejorado error de angle_distance con NEAT

--- python-neat/angle_distance/a_d.py
+++ python-neat/angle_distance/a_d.py
@@ -13,6 +13,7 @@ dataset_full_path = dataset_path+".txt"
 dataset_train = []
 dataset_test = []
 
+train_registers = 1000
 
 def load_dataset(path):
     """
@@ -40,7 +41,7 @@ def load_dataset(path):
 
 def fitness_function(genomes, config):
     for genome_id, genome in genomes:
-        genome.fitness = 1000*5.84
+        genome.fitness = 50000
         distance_difference = 0
         angle_difference = 0
         net = neat.nn.FeedForwardNetwork.create(genome, config)
@@ -64,21 +65,21 @@ def run(config_file):
     #p.add_reporter(stats)
     #p.add_reporter(neat.Checkpointer(5))
 
-    winner = p.run(fitness_function, 50)
+    winner = p.run(fitness_function, 100)
 
     print('\nBest genome:\n{!s}'.format(winner))
 
     print("Output:")
     winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
     error = 0.0
-    distance_difference = 0
-    angle_difference = 0
+    distance_difference = 0.0
+    angle_difference = 0.0
     for xi, xo in dataset_test[:200]:
         output = winner_net.activate(xi)
         distance_difference += (output[0]-xo[0])**2
         angle_difference += (output[1]-xo[1])**2
 
-        #print("expected output ({!r}, {!r}), got ({!r}, {!r})".format(xo[0],xo[1], output[0], output[1]))
+        print("expected output ({!r}, {!r}), got ({!r}, {!r})".format(xo[0],xo[1], output[0], output[1]))
     error = distance_difference + angle_difference
     error = error/400
     error = math.sqrt(error)
@@ -89,8 +90,8 @@ def main():
     local_dir = "./"
     config_path = os.path.join(local_dir, "config.txt")
     dataset_train = load_dataset(dataset_full_path)
-    dataset_test = dataset_train[1000:]
-    dataset_train = dataset_train[:1000]
+    dataset_test = dataset_train[train_registers:]
+    dataset_train = dataset_train[:train_registers]
     run(config_path)
     
 
